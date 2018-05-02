d7a8899a1fd77b00c7a78e468beb6fc268b684ef
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-05-01 22:03:28 -0400

    Feat: Download OSM and GTFS files

--- r5_only.sh
+++ r5_only.sh
@@ -4,3 +4,11 @@ sudo yum update -y
 # Install java 1.8 and remove 1.7
 sudo yum -y install java-1.8.0-openjdk
 sudo yum remove java-1.7.0-openjdk -y
+
+# Download OSM and GTFS files
+cd /home/ec2-user
+mkdir delaware_r5
+cd delaware_r5
+
+curl http://download.geofabrik.de/north-america/us/delaware-latest.osm.pbf -o delaware-latest.osm.pbf
+curl -LO http://dartfirststate.com/information/routes/gtfs_data/dartfirststate_de_us.zip -o dartfirststate_de_us.zip
