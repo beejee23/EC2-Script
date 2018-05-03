d428c4274add99caf4a2617c464bb77fef0cdc26
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-05-02 22:55:22 -0400

    Feat: Add steps to install node and npm

--- modeify.sh
+++ modeify.sh
@@ -3,3 +3,8 @@ sudo yum update -y
 
 # Install git
 sudo yum install git -y
+
+# Install node and npm
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
+sudo yum install -y nodejs
