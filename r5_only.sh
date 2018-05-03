19201228ccb1033bbf6fb5465a5c0cf6cf688614
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-05-02 22:56:49 -0400

    Fix: Remove commands required for modeify
    Removed commands, which are required for modeify.
    Created separate script for modeify.

--- r5_only.sh
+++ r5_only.sh
@@ -12,9 +12,6 @@ cd /home/ec2-user/delaware_r5
 curl http://download.geofabrik.de/north-america/us/delaware-latest.osm.pbf -o delaware-latest.osm.pbf
 curl -LO http://dartfirststate.com/information/routes/gtfs_data/dartfirststate_de_us.zip -o dartfirststate_de_us.zip
 
-# Install git
-sudo yum install git -y
-
 # Download the r5build from public bucket
 cd /home/ec2-user
 aws s3 sync s3://r5-3.2.0 r5build
@@ -23,14 +20,3 @@ aws s3 sync s3://r5-3.2.0 r5build
 cd /home/ec2-user/r5build
 sudo java -Xmx1G -cp target/v3.4.1.jar com.conveyal.r5.R5Main point --build /home/ec2-user/delaware_r5
 sudo java -Xmx1G -cp target/v3.4.1.jar com.conveyal.r5.R5Main point --graphs /home/ec2-user/delaware_r5
-
-# Install node and npm
-sudo yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
-sudo yum install -y nodejs
-
-# Clone conveyal/modeify.git
-cd /home/ec2-user
-git clone https://github.com/conveyal/modeify.git modeify-v1.3.0
-cd modeify-v1.3.0
-git checkout tags/v1.3.0
