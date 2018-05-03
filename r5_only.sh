2bede3af0cfc584674464481c42315f619b5cd29
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-05-02 20:52:27 -0400

    Fix: Change the s3 bucket to get the r5 build file

--- r5_only.sh
+++ r5_only.sh
@@ -16,4 +16,4 @@ curl -LO http://dartfirststate.com/information/routes/gtfs_data/dartfirststate_d
 sudo yum install git -y
 
 # Download the r5build from public bucket
-aws s3 sync s3://commuteplusde/r5build r5build
+aws s3 sync s3://r5-3.2.0 r5build
