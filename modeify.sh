f1c1d2df4efc5b0d2ec694ea11281e6d15c11aa5
Dhanshree Sonar <sonardhanshree@gmail.com>
2018-05-02 22:55:58 -0400

    Feat: Add steps to clone modeify

--- modeify.sh
+++ modeify.sh
@@ -8,3 +8,9 @@ sudo yum install git -y
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
 sudo yum install -y nodejs
+
+# Clone conveyal/modeify.git
+cd /home/ec2-user
+git clone https://github.com/conveyal/modeify.git modeify-v1.3.0
+cd modeify-v1.3.0
+git checkout tags/v1.3.0
